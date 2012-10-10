7b947ea20566e12b83027ac30fa2c1909273759c
Eldorados <mjmcg1@hotmail.com>
2012-10-10 16:35:00 -0400

    Updated for changes to site layout Removed Animated Plinkett from main menu

--- plugin.video.redlettermedia/default.py
+++ plugin.video.redlettermedia/default.py
@@ -45,55 +45,64 @@ def get_http_error(url):
                            
 if play:
 
-    html = get_http_error(url)
-      
-    #First check if there are multiple video parts on the page
-    parts = re.compile('>([PARTart]* [1-9]):<br />').findall(html)
+    #Check if url is youtube link first
+    isyoutube = re.search('youtube.com', url)
     
-    #Page has multiple video parts
-    if len(parts) > 1:
-        partlist = []
-        for part in parts:
-            partlist.append(part)    
-        
-        dialog = xbmcgui.Dialog()
-        index = dialog.select('Choose the video', partlist)
-        
-        #Take only selected part portion of the html
-        if index >= 0:          
-            html = re.search('>%s:<br />(.+?)</p>' % partlist[index],html,re.DOTALL).group(1)
-        else:
-            html = False
+    if isyoutube:
+        stream_url = urlresolver.HostedMediaFile(url).resolve()
     
-    if html:                 
-    
-        #Check for youtube video first
-        youtube = re.search('src="(http://www.youtube.com/[v|embed]*/[0-9A-Za-z_\-]+).+?"',html)      
-        
-        if youtube:
-            stream_url = urlresolver.HostedMediaFile(url=youtube.group(1)).resolve()
-        else:
-        
-            video = re.search('<embed.+?src="http://[a.]{0,2}blip.tv/[^#/]*[#/]{1}([^"]*)"',html, re.DOTALL).group(1)
-            api_url = APIPath % video
-           
-            links = []
-            roles = []
-                
-            tree = parse(urllib.urlopen(api_url))
-            for media in tree.getiterator('media'):
-                for link in media.getiterator('link'):
-                    links.append(link.get('href'))
-                    roles.append(media.findtext('role'))
-                
-            dialog = xbmcgui.Dialog()
-            index = dialog.select('Choose a video source', roles)          
-            if index >= 0:
-                stream_url = links[index]
-            else:
-                stream_url = False
+    #Is a redlettermedia url, so need to find and parse vide link
     else:
-        stream_url = False
+    
+        html = get_http_error(url)
+          
+        #First check if there are multiple video parts on the page
+        parts = re.compile('>([PARTart]* [1-9]):<br />').findall(html)
+        
+        #Page has multiple video parts
+        if len(parts) > 1:
+            partlist = []
+            for part in parts:
+                partlist.append(part)    
+            
+            dialog = xbmcgui.Dialog()
+            index = dialog.select('Choose the video', partlist)
+            
+            #Take only selected part portion of the html
+            if index >= 0:          
+                html = re.search('>%s:<br />(.+?)</p>' % partlist[index],html,re.DOTALL).group(1)
+            else:
+                html = False
+    
+        if html:                 
+        
+            #Check for youtube video first
+            youtube = re.search('src="(http://www.youtube.com/[v|embed]*/[0-9A-Za-z_\-]+).+?"',html)      
+            
+            if youtube:
+                stream_url = urlresolver.HostedMediaFile(url=youtube.group(1)).resolve()
+            else:
+            
+                video = re.search('<embed.+?src="http://[a.]{0,2}blip.tv/[^#/]*[#/]{1}([^"]*)"',html, re.DOTALL).group(1)
+                api_url = APIPath % video
+               
+                links = []
+                roles = []
+                    
+                tree = parse(urllib.urlopen(api_url))
+                for media in tree.getiterator('media'):
+                    for link in media.getiterator('link'):
+                        links.append(link.get('href'))
+                        roles.append(media.findtext('role'))
+                    
+                dialog = xbmcgui.Dialog()
+                index = dialog.select('Choose a video source', roles)          
+                if index >= 0:
+                    stream_url = links[index]
+                else:
+                    stream_url = False
+        else:
+            stream_url = False
     
     #Play the stream
     if stream_url:
@@ -103,7 +112,7 @@ if play:
 def mainpage_links():
     addon.add_directory({'mode': 'none'}, {'title': '[COLOR blue]Recent Updates[/COLOR]'}, is_folder=False, img='')
     html = net.http_GET(MainUrl).content
-    entries = re.compile('<h2 class="entry-title"><a href="(.+?)" title=".+?">(.+?)</a></h2>').findall(html)
+    entries = re.compile('<h2 class="post-title"><a href="(.+?)"[ rel="bookmark"]* title=".+?">(.+?)</a></h2>').findall(html)
     for link, title in entries:
         addon.add_video_item({'url': link},{'title':title})
 
@@ -113,14 +122,13 @@ if mode == 'main':
     addon.add_directory({'mode': 'halfbag', 'url': MainUrl + 'half-in-the-bag/'}, {'title': 'Half in the Bag'}, img=IconPath + 'halfbag.jpg')
     addon.add_directory({'mode': 'featurefilms', 'url': MainUrl + 'films/'}, {'title': 'Feature Films'})
     addon.add_directory({'mode': 'shortfilms', 'url': MainUrl + 'shorts/'}, {'title': 'Short Films'})
-    addon.add_directory({'mode': 'animatedplinkett', 'url': MainUrl + 'mr-plinkett-the-animated-series/'}, {'title': 'Mr. Plinkett - The Animated Series'})
     mainpage_links()
 
 elif mode == 'plinkett':
     url = addon.queries['url']
     html = get_http_error(url)
     
-    r = re.search('Plinkett Reviews</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
+    r = re.search('MR. PLINKETT</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
     if r:
         match = re.compile('<li.+?><a href="(.+?)">(.+?)</a></li>').findall(r.group(1))
     else:
@@ -134,7 +142,8 @@ elif mode == 'plinkettreviews':
     url = addon.queries['url']
     html = get_http_error(url)
 
-    match = re.compile('<td.+?<a href="(.+?)".+?img src="(.+?)"').findall(html)
+    section = re.search('<h1 class="page-title">.+?</h1>(.+?)<script type="text/javascript">', html, re.DOTALL).group(1)
+    match = re.compile('<a href="(.+?)"><img src="(.+?)">').findall(section)
     for link, thumb in match:
         name = re.search("[http://]*[a-z./-]*/(.+?)/",'/' + link).group(1).replace('-',' ').replace('/',' ').title()
         
@@ -159,8 +168,9 @@ elif mode == 'halfbag-episodes':
     url = addon.queries['url']
     html = get_http_error(url)
 
-    match = re.compile('<td[ width=270]*>.*?<a href="(.+?)"><img src="(.+?)"></a></td>', re.DOTALL).findall(html)
-    for link, thumb in match:
+    section = re.search('<h1 class="page-title">.+?</h1>(.+?)<script type="text/javascript">', html, re.DOTALL).group(1)
+    match = re.compile('<a href="(http://[www.]*(redlettermedia|youtube)\.com/[a-zA-Z0-9-/_?=]*[/]*)"[ target=0]*><img src="(.+?jpg)"></a>', re.DOTALL).findall(section)
+    for link, blank, thumb in match:
         episodenum = re.search('([0-9]+)[.]jpg', thumb)
         if episodenum:
             episode_name = 'Episode ' + str(episodenum.group(1))
@@ -180,7 +190,7 @@ elif mode == 'featurefilms':
     url = addon.queries['url']
     html = get_http_error(url)
     
-    r = re.search('Feature Films</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
+    r = re.search('FEATURE FILMS</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
     if r:
         match = re.compile('<li.+?<a href="(.+?)">(.+?)</a></li>').findall(r.group(1))
     else:
@@ -208,7 +218,7 @@ elif mode == 'shortfilms':
     url = addon.queries['url']
     html = get_http_error(url)
 
-    r = re.search('''Short Films and Web Series</a>.+?<ul class="sub-menu">(.+?)</ul>''', html, re.DOTALL)
+    r = re.search('SHORTS AND WEB VIDEOS</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
     if r:
         match = re.compile('<a href="(.+?)">(.+?)</a></li>').findall(r.group(1))
             
@@ -231,17 +241,6 @@ elif mode == 'shortseason':
             name = link.replace(url,'').replace('-',' ').replace('/',' ').title()
             link = url + link.replace(url,'')
             addon.add_video_item({'url': link},{'title': name},img=thumb)
-
-
-elif mode == 'animatedplinkett':
-    url = addon.queries['url']
-    html = get_http_error(url)
-
-    match = re.compile('<td><a href="(.+?)"><img src="(.+?)" width=260></a></td>', re.DOTALL).findall(html)
-    for link, thumb in match:
-        filename = re.search('[^/]+$', thumb).group(0)
-        episode_name = re.search('(.+?)[.]jpg', filename).group(1).replace('_',' ').title()
-        addon.add_video_item({'url': link},{'title': episode_name},img=thumb)
 
 
 if not play:
