ce1bfde83430d835037119133c471feefb7709a4
Eldorados <eldorado.xbmc@gmail.com>
2014-06-26 16:45:19 -0400

    Major addon updates and fixes Added Best of the Worst section

--- plugin.video.redlettermedia/default.py
+++ plugin.video.redlettermedia/default.py
@@ -1,31 +1,40 @@
-import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import xbmc, xbmcgui
 import urllib, urllib2
 import re
 import HTMLParser
-import urlresolver
-from t0mm0.common.addon import Addon
-from t0mm0.common.net import Net
 from elementtree.ElementTree import parse
 
+try:
+    from addon.common.addon import Addon
+    from addon.common.net import Net
+except:
+    xbmc.log('Failed to import script.module.addon.common, attempting t0mm0.common')
+    xbmcgui.Dialog().ok("Import Failure", "Failed to import addon.common", "A component needed by this addon is missing on your system", "Please visit www.xbmc.org for support")
+
+
 addon = Addon('plugin.video.redlettermedia', sys.argv)
-xaddon = xbmcaddon.Addon(id='plugin.video.redlettermedia')
 net = Net()
 
 ##### Queries ##########
 play = addon.queries.get('play', None)
 mode = addon.queries['mode']
 url = addon.queries.get('url', None)
+page_num = addon.queries.get('page_num', None)
 
-print 'Mode: ' + str(mode)
-print 'Play: ' + str(play)
-print 'URL: ' + str(url)
+addon.log('-----------------RedLetterMedia Addon Params------------------')
+addon.log('--- Version: ' + str(addon.get_version()))
+addon.log('--- Mode: ' + str(mode))
+addon.log('--- Play: ' + str(play))
+addon.log('--- URL: ' + str(url))
+addon.log('--- Page: ' + str(page_num))
+addon.log('---------------------------------------------------------------')
 
 
 ################### Global Constants #################################
 
-MainUrl = 'http://www.redlettermedia.com/'
+MainUrl = 'http://redlettermedia.com/'
 APIPath = 'http://blip.tv/players/episode/%s?skin=api'
-AddonPath = xaddon.getAddonInfo('path')
+AddonPath = addon.get_path()
 IconPath = AddonPath + "/icons/"
 
 ######################################################################
@@ -46,13 +55,20 @@ def get_http_error(url):
 
 
 def get_url(url):
+    addon.log('--- Requesting URL: ' + str(url))
     h = HTMLParser.HTMLParser() 
     html = net.http_GET(MainUrl).content
     html = h.unescape(html)
     return html.encode('utf-8')
-     
-                      
+
+
 if play:
+
+    try:
+        import urlresolver
+    except:
+        addon.log_error("Failed to import script.module.urlresolver")
+        xbmcgui.Dialog().ok("Import Failure", "Failed to import URLResolver", "A component needed by this addon is missing on your system", "Please visit www.xbmc.org for support")
 
     #Check if url is youtube link first
     isyoutube = re.search('youtube.com', url)
@@ -86,11 +102,15 @@ if play:
         if html:                 
         
             #Check for youtube video first
-            youtube = re.search('src="(http://www.youtube.com/[v|embed]*/[0-9A-Za-z_\-]+).+?"',html)      
+            youtube = re.search('src="([http:]*//www.youtube.com/[v|embed]*/[0-9A-Za-z_\-]+).+?"',html)
             springboard = re.search('src="(http://cms.springboardplatform.com/.+?)"', html)
             
             if youtube:
-                stream_url = urlresolver.HostedMediaFile(url=youtube.group(1)).resolve()
+                if youtube.group(1).startswith("//"):
+                    youtube_link = 'http:' + youtube.group(1)
+                else:
+                    youtube_link = youtube.group(1)
+                stream_url = urlresolver.HostedMediaFile(url=youtube_link).resolve()
             
             elif springboard:
                 html = net.http_GET(springboard.group(1)).content
@@ -124,24 +144,42 @@ if play:
         addon.resolve_url(stream_url)  
 
 
-def mainpage_links():
+def mainpage_links(page):
     addon.add_directory({'mode': 'none'}, {'title': '[COLOR blue]Recent Updates[/COLOR]'}, is_folder=False, img='')
+    if int(page) > 1:
+        url = MainUrl + 'page/%s/' % page
+    else:
+        url = MainUrl
     html = get_url(url)
-    entries = re.compile('<h2 class="post-title"><a href="(.+?)"[ rel="bookmark"]* title=".+?">(.+?)</a></h2>').findall(html)
-    for link, title in entries:
-        addon.add_video_item({'url': link},{'title':title})
-
+    sections = re.compile('st-[0-9]+ post type-post status-publish format-standard has-post-thumbnail hentry [categorya-z- ]+" id="post-main-[0-9]+">(.+?)(<div class="po|<script)', re.DOTALL).findall(html)
+    for section, nothing in sections:
+        blip_link = re.search('<iframe src="(.+?)"', section)
+        if blip_link:
+            blip_html = net.http_GET(blip_link.group(1)).content
+            icon=re.search('data-episode-thumbnail="(.+?)"', blip_html)
+            thumb='http:' + icon.group(1).replace("THUMB_WIDTH", "630").replace("THUMB_HEIGHT", "350")
+        else:
+            thumb = ''
+        entry = re.search('<h2 class="post-title"><a href="(.+?)"[ rel="bookmark"]* title=".+?">(.+?)</a></h2>', section)
+        addon.add_video_item({'url': entry.group(1)},{'title':entry.group(2)},img=thumb)
+    if re.search('>Next Page', html):
+        page = int(page) + 1
+        addon.add_directory({'mode': 'main_next_page', 'page_num': page}, {'title': 'Next Page(%s)' % page}, img=IconPath + 'next.png')
 
 if mode == 'main': 
     addon.add_directory({'mode': 'plinkett', 'url': MainUrl}, {'title': 'Plinkett Reviews'}, img=IconPath + 'plinkett.jpg')
     addon.add_directory({'mode': 'halfbag', 'url': MainUrl + 'half-in-the-bag/'}, {'title': 'Half in the Bag'}, img=IconPath + 'halfbag.jpg')
+    addon.add_directory({'mode': 'bestworst', 'url': MainUrl + 'best-of-the-worst/'}, {'title': 'Best of the Worst'}, img=IconPath + 'botw-title.jpg')    	
     addon.add_directory({'mode': 'featurefilms', 'url': MainUrl + 'films/'}, {'title': 'Feature Films'})
     addon.add_directory({'mode': 'shortfilms', 'url': MainUrl + 'shorts/'}, {'title': 'Short Films'})
-    mainpage_links()
+    mainpage_links(page=1)
 
+elif mode == 'main_next_page':
+    mainpage_links(page_num)
+    
 elif mode == 'plinkett':
     url = addon.queries['url']
-    html = get_http_error(url)
+    html = get_url(url)
     
     r = re.search('MR. PLINKETT</a>.+?<ul class="sub-menu">(.+?)</ul>', html, re.DOTALL)
     if r:
@@ -201,6 +239,28 @@ elif mode == 'halfbag-episodes':
         addon.add_video_item({'url': link},{'title': episode_name},img=thumb)
 
 
+elif mode == 'bestworst':
+    url = addon.queries['url']
+    html = get_http_error(url)
+
+    section = re.search('<h1 class="page-title">.+?</h1>(.+?)<script type="text/javascript">', html, re.DOTALL).group(1)
+    match = re.compile('<a href="(http://[www.]*(redlettermedia|youtube)\.com/[a-zA-Z0-9-/_?=]*[/]*)"[ target=0]*><img src="(.+?jpg)"></a>', re.DOTALL).findall(section)
+    for link, blank, thumb in match:
+        episodenum = re.search('([0-9.]+)\.jpg', thumb)
+        if episodenum:
+            episode_name = 'Episode ' + str(episodenum.group(1))
+        else:
+            filename = re.search('[^/]+$', thumb).group(0)
+            episode_name = re.search('(.+?)[.]jpg', filename).group(1).replace('_',' ').title()
+        addon.add_video_item({'url': link},{'title': episode_name},img=thumb)
+    
+    shortmatch = re.compile('<a href="(http://www.youtube.com/.+?)" target=0><img src="(.+?)"></a>').findall(html)
+    for link, thumb in shortmatch:
+        filename = re.search('[^/]+$', thumb).group(0)
+        episode_name = re.search('(.+?)[.]jpg', filename).group(1).replace('_',' ').title()
+        addon.add_video_item({'url': link},{'title': episode_name},img=thumb)
+
+
 elif mode == 'featurefilms':
     url = addon.queries['url']
     html = get_http_error(url)
@@ -211,12 +271,12 @@ elif mode == 'featurefilms':
     else:
         match = None
            
-    thumb = re.compile('<td><a href=".+?"><img src="(.+?)"></a></td>').findall(html)
+    thumb = re.compile('<td bgcolor="white" width=200><a href=".+?"><img src="(.+?)"></a></td>').findall(html)
 
     #Add each link found as a directory item
     i = 0
     for link, name in match:
-        addon.add_directory({'mode': 'film', 'url': link}, {'title': name}, img=thumb[i])
+        addon.add_video_item({'url': link}, {'title': name}, img=thumb[i])
         i += 1
 
 elif mode == 'film':
